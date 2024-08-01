13ec10dadaa0ec1c2ed924a92684724a361e1265
SyedIbadSoftwareEngineer <ibad1657@gmail.com>
2024-07-31 20:34:58 -0700

    Project Done

--- components/Form.jsx
+++ components/Form.jsx
@@ -8,7 +8,7 @@ function Form({ type, post, setPost, submitting, handleSubmit }) {
 			</h1>
 			<p className="desc text-left max-w-md">
 				{type} and share amazing prompts with the world, and let your
-				imagination run wild with any AI-powered platform.
+				imagination run wild with any AI-powered platforms.
 			</p>
 			<div className="flex flex-start flex-row w-full">
 				<form
